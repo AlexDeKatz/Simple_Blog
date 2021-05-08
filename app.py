865b1fde779a84e4345d2d025c6991678e3835f7
AlexDeKatz <alecdekatz@gmail.com>
2021-05-08 11:12:40 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flaskblog import app
+from flaskblog import routes
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
